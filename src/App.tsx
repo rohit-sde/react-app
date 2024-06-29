834cef51a7ae5f8046485e073f50655d583a3ea5
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-29 12:20:35 +0530

    render through props

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
     return (
         <div>
-            <Todos />
+            <Todos items={["Learn React", "Learn TypeScript"]} />
         </div>
     );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,24 @@
-function Todos() {
+import React from "react";
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Learn TypeScript</li>
+            {props.items.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
         </ul>
     );
-}
+};
 
 export default Todos;
+
+// function Todos() {
+//     return (
+//         <ul>
+//             <li>Learn React</li>
+//             <li>Learn TypeScript</li>
+//         </ul>
+//     );
+// }
+
+// export default Todos;
