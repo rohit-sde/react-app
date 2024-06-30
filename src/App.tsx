53c841b794dec40d09a064754c73e85164cc04a5
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-30 11:14:03 +0530

    added context

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import Todos from "./components/Todos";
-import Todo from "./models/todo";
-
-// import "./App.css";
 import NewTodo from "./components/NewTodo";
-import { useState } from "react";
+import Todos from "./components/Todos";
+import TodosContextProvider from "./store/todos-context";
+
+// import Todo from "./models/todo";
+// import "./App.css";
 
 function App() {
-    const [todos, setTodos] = useState<Todo[]>([]);
-
-    const addTodoHandler = (todoText: string) => {
-        const newTodo = new Todo(todoText);
-
-        setTodos((prevTodos) => {
-            return prevTodos.concat(newTodo);
-        });
-    };
-
-    const removeTodoHandler = (todoId: string) => {
-        setTodos((prevTodos) => {
-            return prevTodos.filter((todo) => todo.id !== todoId);
-        });
-    };
-
     // const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
     return (
-        <div>
-            <NewTodo onAddTodo={addTodoHandler} />
-            <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-        </div>
+        <TodosContextProvider>
+            <NewTodo />
+            <Todos />
+        </TodosContextProvider>
     );
 }
 
